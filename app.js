43c6e0d0c6f5bf06e45ad3c07cfc4f5d3ec9cfff
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-10-21 21:22:38 +0500

    done leo move

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
+let leoPosition = 10;
+let leoPower = false
+let leoForward = false;
+let leoBackward = false;
+let leo = document.getElementById('leo');
+
 window.addEventListener('keydown' , () =>{
     (event.key == 'ArrowRight') ? moveLeo('forward') :
     (event.key == 'ArrowLeft') ?  moveLeo('Backward') :
+    (event.key == ' ') ?  moveLeo(' ') :
     (event.key == ' ') ? moveLeo(' ') :
     null
 })
 
-const moveLeo = (type) => {
-    let leo = document.getElementById('leo');
+window.addEventListener('keyup', ()=>{
+    leo.src = 'leo.gif';
+    leoForward = false;
+    leoForward = false;
+    leoBackward = false;
+})
 
+const moveLeo = (type) => {
     if(type == 'forward'){
-        console.log(leo.src);
-        
-      leo.src = './leo.forward.gif'
+        if(leoForward == false){
+            leo.src = './leo-forward.gif'
+            leoForward = true
+        }
+        leoPosition += 5
+        leo.style.left = leoPosition + 'px'
+    }
+    else if(type == 'Backward'){
+        if(leoBackward == false){
+            leo.src = './leo-backward.gif'
+            leoBackward = true
+        }
+        leoPosition -= 5
+        leo.style.left = leoPosition + 'px'
+    }
+    else if(type == ' '){
+        // if(leoBackward == false){
+            leo.src = './leo-power.gif'
+            // leoPower = true
+        // }
     }
 }
